5ad75d43e4bc1fbe14f160c8053270845349ca13
tanmay7256 <tanmayarora1112@gmail.com>
2022-07-16 17:34:03 +0530

    after html of home login cart

--- server.js
+++ server.js
@@ -5,9 +5,7 @@ const expresslayout = require('express-ejs-layouts')
 const path = require('path')
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-    res.render('home')
-})
+
 
 
 // set-template
@@ -15,7 +13,20 @@ app.use(expresslayout)
 app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
 
+app.get('/', (req, res) => {
+    res.render('home')
+})
+app.get('/cart', (req, res) => {
+    res.render('customers/cart')
+})
+app.get('/login', (req, res) => {
+    res.render('auth/login')
+})
+app.get('/register', (req, res) => {
+    res.render('auth/register')
+})
 
+app.use(express.static('public'));
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
 })
